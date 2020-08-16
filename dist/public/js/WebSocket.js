f604baaeef00c00c935c46f8da71e0d1910606e5
Laxelott <laxelott@hotmail.com>
2020-08-16 03:36:29 -0500

    Reordered dist files Added main video seeking functionality Finished Web Socket functionality Perfected app.js normal and socket server

---
+++ dist/public/js/WebSocket.js
@@ -0,0 +1,33 @@
+var webSocket = {
+    socket: null,
+    webSocketLocation: window.location.host,
+    connect: function() {
+        webSocket.socket = new WebSocket(webSocket.getFullPath());
+        webSocket.socket.onopen = webSocket.onOpen;
+        webSocket.socket.onerror = webSocket.onError;
+        webSocket.socket.onclose = webSocket.onClose;
+        webSocket.socket.onmessage = webSocket.onMessage;
+    },
+    onOpen: function() {
+        console.log("Connected");
+    },
+    onMessage: function(msg) {
+        console.log("Message Received:");
+        console.log(msg);
+    },
+    onError: function(error) {  
+        console.log("Error!");
+        console.log(error);
+    },
+    onClose: function() {
+        
+    },
+    send: function(content) {
+        webSocket.socket.send(content);
+    },
+    getFullPath: function() {
+        return `ws://${this.webSocketLocation}`;
+    }
+};
+
+export { webSocket };
--- src/app.js
+++ src/app.js
@@ -7,30 +7,39 @@ const server = http.createServer(app);
 const wss = new ws.Server({ server: server });
 
 const webPort = 8080;
-const socketPort = 8081;
-
-
 
 
 // WebSocket listener
-wss.on('connection', (socket, request) => {
-	// Send Callback
-	socket.send("Callback.. Hellooo!");
+wss.on('connection', (ws) => {
+	var data = {
+		event: "handshake",
+		data: "hello! C:"
+	};
 
-	console.log(`${request.connection.address} connected!`);
+	// Send Callback
+	ws.send(JSON.stringify(data));
+
+	console.log('WS Connection from %s:%d', ws._socket.remoteAddress, ws._socket.remotePort);
 
 	// Add a simple message event
-	socket.on('message', (message) => {
-		// Log the received message and send it back to the client
-		console.log('received: %s', message);
+	ws.on('message', (message) => {
+		data = JSON.parse(message);
 
-		// Broadcast message to everyone 
+		// Log the received message and send it back to the client
+		console.log('Received: %s', message);
+
+		// Add timestamp
+		data.timestamp = new Date().getTime() + 500;
+
+		// Broadcast data to everyone 
 		wss.clients.forEach(client => {
-			client.send(`Hello, broadcast message -> ${message}`);
+			client.send(JSON.stringify(data));
 		});
 	});
 
 });
+
+
 
 //Static server
 app.use(express.static("dist/public"));
@@ -40,11 +49,8 @@ app.get('/', (req, res) => {
 });
 
 
-// Seting up servers
-app.listen(webPort, () => {
-	console.log(`Normal server open on port ${webPort}!`);
-});
 
-server.listen(socketPort, () => {
-	console.log(`Websocket server open on port ${socketPort}!`);
+// Seting up server
+server.listen(webPort, () => {
+	console.log(`Server opened on port ${webPort}!`);
 })
